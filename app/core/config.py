11b66ef525961cc406b0bc03adc4d2f621d1d3be
maithoa <maithoa@example.emaildomain>
2026-09-25 13:52:25 +0300

    feat: Build AsyncDB engine and plug it in lifespan in main.py

--- app/core/config.py
+++ app/core/config.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     VERSION: str = "0.1.0"
     API_V1_STR: str= "/api/v1"
     PORT: int=8686
+    DATASE_URL = "sqlite+aiosqlite:///./chat_playground.db"
 
 #  Use Pydantic V2 model config
     model_config = SettingsConfigDict(
--- app/main.py
+++ app/main.py
@@ -7,15 +7,23 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.concurrency import asynccontextmanager
 from app.core.config import settings
+from app.core.db import init_db
 from app.api.v1.router import api_router
 
+# Lifespan event handlers : Automatically create connection and create SQLite tables when the application starts up
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup: Automatically create database tables if they are not existing
+    await init_db()
+    yield
 
-# 1. Init FastAPI app
+# Init FastAPI app
 app= FastAPI(
     title=settings.APP_NAME,
-    version=settings.API_V1_STR
-
+    version=settings.API_V1_STR,
+    lifespan=lifespan
 )
 
 # Add CORSMiddleweare separately
@@ -39,7 +47,7 @@ def read_root():
 def health_check():
     return {"status": "ok"}
 
-# 2. Mount API routes
+# Mount API routes
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 if __name__ == "__main__":
